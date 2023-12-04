bc4cfa08f8b2e7702531ec60dec01b0f71a4d41f
Tiziano Di Felice <tizianofelice@gmail.com>
2023-12-04 12:32:02 +0100

    stato delle chips redux

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,13 +1,18 @@
 import { useEffect} from 'react';
-import './Player.css'
+import './Player.css';
+// import { addChips, removeChips } from '../../state/Chips/ChipsSlice';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { RootState } from '../../state/store';
+// import { useDispatch } from 'react-redux';
 
 function Player(props:{isUser: boolean}){
 
     useEffect(()=>{
     }, [])
 
-    const chips: number = 100; 
+    const chips = useSelector((state: RootState)=> state.chips.chips) 
     const stylePlayer = props.isUser ? 'user' : 'player';
+    // const dispatch = useDispatch();
 
     return (
         <>
