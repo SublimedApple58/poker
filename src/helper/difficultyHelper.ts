95790f4d3a2070665eb0f3391d5d6bcdee2fb2c3
Tiziano Di Felice <tizianofelice@gmail.com>
2024-01-30 20:04:33 +0100

    difficultyHelper completo

--- src/helper/difficultyHelper.ts
+++ src/helper/difficultyHelper.ts
@@ -31,18 +31,10 @@ class difficultyHelper{
 
         let 
             mossa: Moves = call,
-            bet: number | undefined;
+            bet: number | undefined,
+            bluff: boolean = player.bluff;
 
         const finished: boolean = player.finished;
-
-        switch(difficulty){
-            case Difficulty.easy:
-                break;
-            case Difficulty.medium: 
-                break;
-            case Difficulty.hard:
-                break;
-        }
         
         if(round==1){
             if(casualNumber(7) == 1){
@@ -76,24 +68,116 @@ class difficultyHelper{
         } else {
             switch(true){
                 case score <= 14: // nulla
-                    if(call == Moves.check){
-                        mossa = call;
+                    if(bluff && (difficulty == Difficulty.medium || difficulty == Difficulty.hard)){
+                        if(difficulty == Difficulty.hard){
+                            if(casualNumber(14) == 1){
+                                if(finished){
+                                    mossa = Moves.check
+                                } else {
+                                    mossa = Moves.allIn;
+                                }
+                            } else {
+                                if(finished){
+                                    if(higher - player.bet > player.chips){
+                                        mossa = Moves.fold;
+                                    } else {
+                                        mossa = call
+                                    }
+                                } else {
+                                    ({ mossa, bet } = tryRaise(20, 60));
+                                    if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*60){
+                                        mossa = Moves.fold;
+                                    }
+                                }
+                            }
+                        } else if(finished){
+                            if(player.chips - (higher - player.bet) < player.chips/100*70){
+                                mossa = Moves.fold;
+                            } else {
+                                mossa = call
+                            }
+                        } else {
+                            ({ mossa, bet } = tryRaise(10, 70));
+                            if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*70){
+                                mossa = Moves.fold;
+                            }
+                        }
                     } else {
-                        mossa = Moves.fold;
+                        if(call == Moves.check){
+                            mossa = call;
+                        } else {
+                            mossa = Moves.fold;
+                        }
                     }
                     break;
                 case score >= 15 && score < 30: // coppia
-                    if(higher - player.bet > (player.chips/100*10)){
-                        mossa = Moves.fold;
+                    if(bluff && (difficulty == Difficulty.medium || difficulty == Difficulty.hard)){
+                        if(difficulty == Difficulty.hard){
+                            if(finished){
+                                if(higher - player.bet > player.chips){
+                                    mossa = Moves.fold;
+                                } else {
+                                    mossa = call
+                                }
+                            } else {
+                                ({ mossa, bet } = tryRaise(30, 60));
+                                if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*60){
+                                    mossa = Moves.fold;
+                                }
+                            }
+                        } else if(finished){
+                            if(higher - player.bet > player.chips){
+                                mossa = Moves.fold;
+                            } else {
+                                mossa = call
+                            }
+                        } else {
+                            ({ mossa, bet } = tryRaise(10, 70));
+                                if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*60){
+                                    mossa = Moves.fold;
+                                }
+                        }
                     } else {
-                        mossa = call;
+                        if(higher - player.bet > (player.chips/100*10)){
+                            mossa = Moves.fold;
+                        } else {
+                            mossa = call;
+                        }
                     }
                     break;
                 case score >= 30 && score < 45: // doppia coppia
-                    if(higher - player.bet > (player.chips/100*20)){
-                        mossa = Moves.fold;
+                    if(bluff && (difficulty == Difficulty.medium || difficulty == Difficulty.hard)){
+                        if(difficulty == Difficulty.hard){
+                            if(finished){
+                                if(higher - player.bet > player.chips){
+                                    mossa = Moves.fold;
+                                } else {
+                                    mossa = call
+                                }
+                            } else {
+                                ({ mossa, bet } = tryRaise(20, 60));
+                                if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*60){
+                                    mossa = Moves.fold;
+                                }
+                            }
+                        } else if(finished){
+                            if(higher - player.bet > player.chips){
+                                mossa = Moves.fold;
+                            } else {
+                                mossa = call
+                            }
+                        } else {
+                            ({ mossa, bet } = tryRaise(20, 60));
+                            if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*60){
+                                mossa = Moves.fold;
+                            }
+                        }
                     } else {
-                        mossa = call;
+                        if(higher - player.bet > (player.chips/100*20)){
+                            mossa = Moves.fold;
+                        } else {
+                            mossa = call;
+                        }
                     }
                     break;
                 case score >= 45 && score < 60: // tris
@@ -118,9 +202,17 @@ class difficultyHelper{
                             mossa = call
                         }
                     } else {
-                        ({ mossa, bet } = tryRaise(20, 50));
-                        if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*50){
-                            mossa = Moves.fold;
+                        if(difficulty == Difficulty.hard || difficulty == Difficulty.medium){
+                            if(finished){
+                                mossa = Moves.check;
+                            } else {
+                                mossa = Moves.allIn;
+                            }
+                        } else {
+                            ({ mossa, bet } = tryRaise(20, 50));
+                            if(mossa == Moves.call && player.chips - (higher - player.bet) < player.chips/100*50){
+                                mossa = Moves.fold;
+                            }
                         }
                     }
                     break;
@@ -133,11 +225,7 @@ class difficultyHelper{
                     break;
             }
         }
-        if(bet){
-            return {mossa, bet}
-        } else {
-            return mossa
-        }
+        return { mossa, bet}
     }
 }
 
